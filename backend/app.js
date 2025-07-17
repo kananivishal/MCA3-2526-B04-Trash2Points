b66364c707fac3af82c5ff9f2549ad312633d02a
Kanani Vishal <kananivishal98@gmail.com>
2025-07-18 02:24:39 +0530

    Auth Api Completed

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
 const express = require("express")
+// Server Creatation
 const app = express()
 const PORT = 8080
+// Database Connection
+const connectDB = require('./DB/connectDB')
+// middleware require
+const cors = require('cors')
+const morgan = require('morgan')
+const routes = require('./routes/index')
 
-app.get("/", (req,res)=>
-    res.send("Hello")
-)
+connectDB()
+
+// middleware
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.use(routes)
 
 app.listen(PORT, ()=>
     console.log(`http://localhost:${PORT}`)
---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,97 @@
+const { User } = require('../model/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const register = async (req, res) => {
+    try {
+        let { name, email, phoneno, address, password, role } = req.body
+
+        if (!name || !email || !phoneno || !address || !password || !role) {
+            res.status(400).json({
+                message: "Some fields are missing!"
+            })
+        }
+        const isUserAlreadyExist = await User.findOne({ email })
+        if (isUserAlreadyExist) {
+            return res.status(400).json({
+                message: "User already exist!"
+            })
+        }
+
+        // hash the password
+        const salt = bcrypt.genSaltSync(10)
+        const passwordHashed = bcrypt.hashSync(password, salt)
+
+        // jwt token
+        const token = jwt.sign({ email }, "Trush2Points", { expiresIn: '365d' })
+
+        // create user
+        await User.create({
+            name,
+            email,
+            phoneno,
+            address,
+            // password: passwordHashed,
+            password,
+            role: 'user'
+        })
+
+        res.status(200).json({
+            message: "User created successfually",
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error!"
+        })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        let { email, password, role } = req.body
+
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Some fields are missing!"
+            })
+        }
+        let user = await User.findOne({ email })
+        if (!user) {
+            res.status(400).json({
+                message: "User not registerd!"
+            })
+        }
+        // compare password
+        // const isPasswordMatch = bcrypt.compareSync(password, user.password)
+        // if (!isPasswordMatch) {
+        //     return res.status(400).json({
+        //         message: "Password not matched"
+        //     })
+        // }
+
+        if (password != user.password) {
+            return res.status(400).json({
+                message: "Password not matched!"
+            })
+        }
+
+        // jwt token
+        const token = jwt.sign({ email }, "Trush2Points", { expiresIn: '365d' })
+        
+        res.send({
+            message: "User logged in",
+            user,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Internal server error!"
+        })
+    }
+}
+
+module.exports = { register, login }
